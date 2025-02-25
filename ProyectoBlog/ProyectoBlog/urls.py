947542aabb1e634a045dbdf49120056f908aa82f
gabrielLinux <gabrielcabj986@gmail.com>
2025-02-25 10:12:34 -0300

    Practica matutina

--- ProyectoBlog/ProyectoBlog/urls.py
+++ ProyectoBlog/ProyectoBlog/urls.py
@@ -17,11 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from mainApp import views
+import pages.views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index,name='index'),
     path('inicio/',views.index,name='inicio'),
-    path('sobre-nosotros/',views.index,name='about')
+    path('pagina/<str:slug>',pages.views.page,name='page')
 ]
 
--- ProyectoBlog/pages/views.py
+++ ProyectoBlog/pages/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from .models import Page
 
 # Create your views here.
-def page(request):
+def page(request,slug):
+    page = Page.objects.get(slug=slug)
 
-    return render(request,"pages/page.html",{"page": "Hola mundo desde la app pags"})
+    return render(request,"pages/page.html",{
+        "page": page
+        })
